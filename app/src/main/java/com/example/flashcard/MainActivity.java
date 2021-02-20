620bb1e58db3e222d1f80dcfc51e53dd45ee7f3f
Tri Truong <truongductri01@gmail.com>
2021-02-20 13:30:46 -0600

    Finish additional tasks

--- app/src/main/java/com/example/flashcard/MainActivity.java
+++ app/src/main/java/com/example/flashcard/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.flashcard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -9,6 +10,9 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     TextView flashcard_question;
     TextView flashcard_answer;
+    TextView answer1;
+    TextView answer2;
+    TextView answer3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
 
         flashcard_question = findViewById(R.id.flashcard_question);
         flashcard_answer = findViewById(R.id.flashcard_answer);
+        answer1 = findViewById(R.id.first_answer);
+        answer2 = findViewById(R.id.second_answer);
+        answer3 = findViewById(R.id.third_answer);
 
         flashcard_question.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +38,28 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 flashcard_question.setVisibility(View.VISIBLE);
                 flashcard_answer.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        answer1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            answer1.setBackgroundColor(getResources().getColor(R.color.lime_green));            }
+        });
+
+        answer2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                answer1.setBackgroundColor(getResources().getColor(R.color.lime_green));
+                answer2.setBackgroundColor(getResources().getColor(R.color.crimson));
+            }
+        });
+
+        answer3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                answer1.setBackgroundColor(getResources().getColor(R.color.lime_green));
+                answer3.setBackgroundColor(getResources().getColor(R.color.crimson));
             }
         });
     }
