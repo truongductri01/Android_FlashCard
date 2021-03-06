0a30fd7b577e4e163e0b774452fc67131566e903
Tri Truong <truongductri01@gmail.com>
2021-03-06 13:22:04 -0600

    Add ImageView to Cancel AddCardActivity

--- app/src/main/java/com/example/flashcard/AddCardActivity.java
+++ app/src/main/java/com/example/flashcard/AddCardActivity.java
@@ -3,12 +3,22 @@ package com.example.flashcard;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class AddCardActivity extends AppCompatActivity {
-
+    ImageView cancelBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_card);
+
+        cancelBtn = findViewById(R.id.cancelCardBtn);
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
